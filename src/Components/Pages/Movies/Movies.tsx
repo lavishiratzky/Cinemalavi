18cd803deb1815112199a614bf92c1299634ba44
lavi shiratzky <lavi,shiratzky@gmail.com>
2023-04-30 16:04:42 +0300

    repair of select input

--- src/Components/Pages/Movies/Movies.tsx
+++ src/Components/Pages/Movies/Movies.tsx
@@ -9,7 +9,7 @@ import urlService from "../../../Services/UrlServices";
 import { gotAllMoviesAction } from "../../../Redux/MoviesAppState";
 function Movies(): JSX.Element {
     const[movies,setMovies] = useState<MovieModel[]>(store.getState().moviesReducer.movies);
-    // const [selectedGenre, setSelectedGenre]= useState<string>("none")
+    const [selectedGenre, setSelectedGenre]= useState<string>("none")
     const [searchTerm,setSearchTerm]= useState<string>("")
     // const url ="https://raw.githubusercontent.com/Adiper84/moviesDB/main/moviesdb.txt"
     useEffect(() => {
@@ -25,34 +25,28 @@ function Movies(): JSX.Element {
             notifyService.failure("Data was Not collected!!")})
         }
     },[])
-    // const selectGenre =(event: React.ChangeEvent<HTMLSelectElement>) =>{
-    //     const value =event.target.value;
-    //     setSelectedGenre(value)
+    const selectGenre =(event: React.ChangeEvent<HTMLSelectElement>) =>{
+        const value =event.target.value;
+        console.log({event,value});
+        setSelectedGenre(value);
+    }
     
     return (
         <div className="Movies">
             <h1><input type ="text" placeholder="Type a name of a movie" onChange={event=>{setSearchTerm(event.target.value)}}/></h1>
-        {/* //     <h1> <select onChange={selectGenre}>
-        //     <option value="none">Filter by Genre</option>
-        //     <option value="Action">Action</option>
-        //     <option value="Crime">Crime</option>
-        //     <option value="Drama">Drama</option>
-        //     <option value="Romance">Romance</option>
-        //     <option value="History">History</option>
-        //     <option value="Fantasy">Fantasy</option>
-        //     <option value="Adventure">Adventure</option>
-        //     <option value="Thriller">Thriller</option>
-        //     <option value="Sci-Fi">Sci-Fi</option>
-        // </select></h1> } */}
-            {/* {movies.filter((val)=>{
-                if(val.genre===selectedGenre ) {
-                    return true
-                }
-                else if(selectedGenre ==='none') {
-                    return true
-                }
-            }
-            ).map(val=><MovieCard key={'movie' + val.id} movie={val}/>)}) */}
+             <h1> <select onChange={selectGenre}>
+             <option value="none">Filter by Genre</option>
+             <option value="Action">Action</option>
+             <option value="Crime">Crime</option>
+             <option value="Drama">Drama</option>
+             <option value="Romance">Romance</option>
+             <option value="History">History</option>
+             <option value="Fantasy">Fantasy</option>
+             <option value="Adventure">Adventure</option>
+             <option value="Thriller">Thriller</option>
+             <option value="Sci-Fi">Sci-Fi</option>
+         </select></h1> 
+            
  {movies.filter((val)=>{
                 if (searchTerm===""){
                     return store.dispatch
@@ -60,8 +54,17 @@ function Movies(): JSX.Element {
                 else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
                     return val
                 }
+                return false;
             }
-            ).map(m=><MovieCard key={'movie' + m.id} movie={m}/>)}
+            ).filter((val)=>{
+                if(val.genre.toLowerCase().split(',').map(v=>v.trim()).includes(selectedGenre.toLowerCase()) ) {
+                    return true
+                }
+                else if(selectedGenre ==='none') {
+                    return true
+                }
+                return false;
+            }).map(m=><MovieCard key={'movie' + m.id} movie={m}/>)}
 {/* TO SHOW ONLY THE FIRST 5 USE THIS BEFORE MAP .slice(0,5) */}
         
 
